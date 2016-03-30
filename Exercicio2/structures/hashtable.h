397fa74d11ce19af8896a1921d8600c94ae620f4
_bpp <pereirabruno05@gmail.com>
2016-03-30 01:20:02 +0100

    Adiciona hashtable para ponto 3 do tp

---
+++ Exercicio2/structures/hashtable.h
@@ -0,0 +1,10 @@
+
+typedef struct item AUTOR;
+
+void add_autor(char *nome_autor);
+void add_coautor(char *nome_autor, char *nome_autor2);
+AUTOR *find_autor(char *nome_autor);
+void delete_autor(AUTOR *autor);
+void delete_all(void);
+void print_autores(void);
+void print_autor(char *nome_autor);
